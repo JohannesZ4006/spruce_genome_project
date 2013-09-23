56a0764407a05c93634b94ae5198ca59fd61453f
JohannesZ4006 <johannes.luthman@scilifelab.se>
2013-09-23 12:00:14 +0200

    Update fastqi2fastqp.pl
    _1/2.fastq changed to .1/2.fq

--- script/fastqi2fastqp.pl
+++ script/fastqi2fastqp.pl
@@ -11,8 +11,8 @@ use strict;
 my ($in, $outID)=@ARGV;
 
 open(IN, $in);
-open(OUT1, ">$outID\_1.fastq");
-open(OUT2, ">$outID\_2.fastq");
+open(OUT1, ">$outID\.1.fq");
+open(OUT2, ">$outID\.2.fq");
 
 while(my $seq1=<IN>){
     $seq1 .= <IN>;
