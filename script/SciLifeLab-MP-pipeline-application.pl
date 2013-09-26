6dfdd04b009d406921a76b9901cac983b5e02cd2
Francesco Vezzi <vezzi@kalkyl1.uppmax.uu.se>
2013-09-26 10:01:25 +0200

    SciLifeLab-MP-pipeline-application now dependent only on CLC software path

--- script/SciLifeLab-MP-pipeline-application.pl
+++ script/SciLifeLab-MP-pipeline-application.pl
@@ -6,15 +6,15 @@ use Pod::Usage;
 use File::Basename;
 
 
-##########################################################################################################
-##########																						##########
-##########                 MATE-PAIRS-APPLICATION PIPELINE										##########
-##########																						##########
-##########################################################################################################
+##############################################################################################################################
+##########                                                                                                          ##########
+##########                                 MATE-PAIRS-APPLICATION PIPELINE                                          ##########
+##########   http://res.illumina.com/documents/products/technotes/technote_nextera_matepair_data_processing.pdf     ##########
+##########                                                                                                          ##########
+##############################################################################################################################
 
-### adapter
+### adapter Nextera Mate Pair library: pay attention, old hybrid Illumina-454 protocol has different adaptor/linkers
 ## -a CTGTCTCTTATACACATCTAGATGTGTATAAGAGACAG -a CTGTCTCTTATACACATCT -a AGATGTGTATAAGAGACAG
-### references /bubo/nobackup/uppnex/reference/biodata/biodata.stage/genomes/Hsapiens/hg19/bwa/
 
 
 
@@ -23,14 +23,12 @@ my @reads_2				= ();
 my @IDs      			= (); ## headers name of all outputs files
 my $histWidth			= 10000;
 my $noAdaptorRemoval	= 0;
-my $noDuplicatesRemoval = 0;
-my $noClean 			= 0;
 my $aligner             = "bwa";
+my @supportedAligners   = ("bwa", "bwaMEM", "bowtie");
 my $reference 			= "";
 my @adaptors            = ();
 my $help  				= 0;
 
-my $scriptDir    		= "/proj/b2013064/vezzi/scripts/projects/MP_pipelineProcessing/";
 
 GetOptions(
     'aligner=s'         => \$aligner,
@@ -39,7 +37,6 @@ GetOptions(
     'adaptor=s'         => \@adaptors,
 	'hist-width=s'		=> \$histWidth,
 	'no-adapt-removal'	=> \$noAdaptorRemoval,
-	'no-clean'     		=> \$noClean,
 	'help|?'    		=> \$help
 ) or pod2usage(2);
 
@@ -49,30 +46,54 @@ pod2usage(1) if $help;
 pod2usage("--reads cannot be left empty: specify at least one  dataset")  if (@reads_1 == 0);
 
 
-
+### If no adaptor is specified use Nextera MP adaptors
 if(@adaptors == 0) {
     @adaptors = ("CTGTCTCTTATACACATCTAGATGTGTATAAGAGACAG", "CTGTCTCTTATACACATCT",  "AGATGTGTATAAGAGACAG");
 }
 
-#my check if files exists and they are provided with the correct mate pair files
+#CHECK if files exists and if they are provided with the correct mate pair files
 my $referenceID = "";
-checkDataConsistency();
-## NOW @IDs contains the base name
+checkDataConsistency(); ## NOW @IDs contains the base name
 
+#CHECK if specified aligner (default is bwa) is supported
+isAlignerSupported();
+
+#CREATE trash directory where mv temporary files (to be deleted manually after a succefull job)
 mkdir "toBeDeleted";
 my @ToBeDeleted = ();
 
+#CHECK if reference is provided and based on this choose which pipeline to run
 if($reference eq "") {
     MPpipeline_noRef();
 } else {
     MPpipeline_Ref();
 }
 
+#MOVE temporary files to directory toBeDeleted
 foreach my $file (@ToBeDeleted) {
     system("mv $file toBeDeleted");
 }
 
 exit 0;
+
+
+#### SUBROUTINES 
+
+sub isAlignerSupported {
+    my $isAlignerSupported = 0;
+    foreach my $tool (@supportedAligners) {
+        if($tool eq $aligner) {
+            $isAlignerSupported = 1;
+        }
+    }
+    if($isAlignerSupported == 0) {
+        print "aligner specified is not supported by the program. Please specify one of the following:\n";
+        foreach my $tool (@supportedAligners) {
+            print "\t$tool\n";
+        }
+        die("aligner $aligner is not supported please specify")
+    }
+}
 
 
 sub MPpipeline_Ref {
@@ -85,13 +106,12 @@ sub MPpipeline_Ref {
         my $read_1   = $reads_1[$i];
         my $read_2   = $reads_2[$i];
         my $ID       = $IDs[$i];
-
+        
+        ##COUNT READS
         my ($numReadsFile1, $numReadsFile2) = countReads($read_1, $read_2);
         print "number of original reads pairs is $numReadsFile1\n";
         
-        ## aling original reads
-        
-        if($noAdaptorRemoval == 0) { #remove adoptor and re-aling
+        if($noAdaptorRemoval == 0) { #remove adoptor and aling
             ($read_1, $read_2, $ID) = removeLinker($read_1, $read_2, $ID);
             if($aligner eq "bwa") {
                 alignReads_bwa($read_1, $read_2, $ID);
@@ -100,7 +120,7 @@ sub MPpipeline_Ref {
             } else {
                 alignReads_bowtie($read_1, $read_2, $ID);
             }
-        } else {
+        } else { # do not remove adaptor
             if($aligner eq "bwa") {
                 alignReads_bwa($read_1, $read_2, $ID);
             } elsif($aligner eq "bwaMEM") {
@@ -195,12 +215,14 @@ sub alignReads_bwa {
     my $Reads2SAI		="$MappingBase\_2.sai";
     my $Reads1SAI_err	="$MappingBase\_1.err";
     my $Reads2SAI_err	="$MappingBase\_2.err";
+    my $SAMTOOLS_view_err	="$MappingBase\_samtools_view.err";
     
     system("bwa aln -t 8 -o 1 $reference $read_1 > $Reads1SAI 2> $Reads1SAI.err") == 0 or die("error:$!");
     system("bwa aln -t 8 -o 1 $reference $read_2 > $Reads2SAI 2> $Reads2SAI.err") == 0 or die("error:$!");
-    system("bwa sampe -P -s $reference $Reads1SAI $Reads2SAI $read_1 $read_2 2> $MappingBase\_sampe.err | samtools view -Shb -F 4 - > $BAMMapped 2>> $MappingBase\_sampe.err") == 0 or die("error:$!");
+    system("bwa sampe -P -s $reference $Reads1SAI $Reads2SAI $read_1 $read_2 2> $MappingBase\_sampe.err | samtools view -Shb -F 4 - > $BAMMapped 2>> $SAMTOOLS_view_err") == 0 or die("error:$!");
     system("rm -f $Reads1SAI $Reads2SAI");
     push @ToBeDeleted, "$MappingBase\_sampe.err";
+    push @ToBeDeleted, $SAMTOOLS_view_err;
     push @ToBeDeleted, "$Reads1SAI.err";
     push @ToBeDeleted, "$Reads2SAI.err";
     
@@ -217,9 +239,11 @@ sub alignReads_bwaMEM {
     my $BAMMapped  = $MappingBase.".unsorted.mapped.bam";
     my $BAMmarkDup = "$MappingBase\_markDup.bam";
     my $BWAmemErr	="$MappingBase\.err";
+    my $SAMTOOLS_view_err	="$MappingBase\_samtools_view.err";
     
     system("bwa mem -M -t 8  $reference $read_1 $read_2 > $SAMMapped 2> $BWAmemErr") == 0 or die("error:$!");
-    system("samtools view -S -F 4 -b $SAMMapped >  $BAMMapped");
+    system("samtools view -S -F 4 -b $SAMMapped >  $BAMMapped 2> $SAMTOOLS_view_err");
+    push @ToBeDeleted, $SAMTOOLS_view_err;
     system("rm $SAMMapped");
     
     sort_duplicates_GCcontentCheck($read_1, $read_2, $outputID)
@@ -234,10 +258,11 @@ sub alignReads_bowtie {
     my $BAMmarkDup = "$MappingBase\_markDup.bam";
     my $BOWTIEerr	="$MappingBase\.err";
     my $BOWTIEout	="$MappingBase\.out";
+    my $SAMTOOLS_view_err	="$MappingBase\_samtools_view.err";
     
-    print "bowtie2 -p 8 -x $reference -1 $read_1 -2 $read_2 -S $SAMMapped 2> $BOWTIEerr > $BOWTIEout";
     system("bowtie2 -p 8 -x $reference -1 $read_1 -2 $read_2 -S $SAMMapped 2> $BOWTIEerr > $BOWTIEout") == 0 or die("error:$!");
-    system("samtools view -S -F 4 -b $SAMMapped >  $BAMMapped");
+    system("samtools view -S -F 4 -b $SAMMapped >  $BAMMapped 2> $SAMTOOLS_view_err ");
+    push @ToBeDeleted, $SAMTOOLS_view_err;
     system("rm $SAMMapped");
     
     sort_duplicates_GCcontentCheck($read_1, $read_2, $outputID)
@@ -247,14 +272,14 @@ sub alignReads_bowtie {
 
 sub sort_duplicates_GCcontentCheck{
     my ($read_1, $read_2, $outputID) = @_;
-    my $MappingBase="$outputID-to-$referenceID";
-    my $BAM        = "$MappingBase.bam";
-    my $SAMMapped  = $MappingBase.".unsorted.mapped.sam";
-    my $BAMMapped  = $MappingBase.".unsorted.mapped.bam";
-    my $BAMmarkDup = "$MappingBase\_markDup.bam";
+    my $MappingBase ="$outputID-to-$referenceID";
+    my $BAM         = "$MappingBase.bam";
+    my $SAMMapped   = $MappingBase.".unsorted.mapped.sam";
+    my $BAMMapped   = $MappingBase.".unsorted.mapped.bam";
+    my $BAMmarkDup  = "$MappingBase\_markDup.bam";
     my $BWAmemErr	="$MappingBase\.err";
     
-    system("samtools sort $BAMMapped $MappingBase");
+    system("samtools sort $BAMMapped $MappingBase 2> $MappingBase\_samtools_sort.err");
     system("samtools index $BAM") == 0 or die("error:$!");
     
     system("rm $BAMMapped");
@@ -270,9 +295,9 @@ sub sort_duplicates_GCcontentCheck{
     system("java -Xmx16g -XX:PermSize=2g -jar \$PICARD_HOME/CollectGcBiasMetrics.jar REFERENCE_SEQUENCE=$reference  INPUT=$BAM OUTPUT=$MappingBase\.collectGcBias.txt CHART_OUTPUT=$MappingBase\.collectGcBias.pdf ASSUME_SORTED=true 2> $MappingBase\_collectGCbias.std > $MappingBase\_collectGCbias.out  ");
 
     
+    push @ToBeDeleted, "$MappingBase\_samtools_sort.err";
     push @ToBeDeleted, "$MappingBase\_collectInsertSize.err";
 	push @ToBeDeleted, "$MappingBase\_MarkDuplicates.err";
-    push @ToBeDeleted, "$MappingBase\_samtools_sort.err";
     push @ToBeDeleted, "$MappingBase\_collectGCbias.std";
     push @ToBeDeleted, "$MappingBase\_collectGCbias.out";
 
@@ -282,6 +307,7 @@ sub sort_duplicates_GCcontentCheck{
 
 sub removeLinker {
     my ($read_1, $read_2, $outputID) = @_;
+    ##TODO: replace CLCadapter-trim with cutadapt
     my $trimTool ="/proj/a2012043/software/clc-adapter-trim/adapter_trim-4.08beta";
     
     print "looking for adapter\n";
@@ -317,9 +343,7 @@ sub removeLinker {
     system("cat $pairedInterlevedLinker $pairedInterlevedNoLinker > $pairedInterleavedTrimmed") == 0 or die("error while running cat $pairedInterlevedLinker $pairedInterlevedNoLinker > $pairedInterleavedTrimmed: $!");
     
     my $tailID .= "_trimmed";
-    system("$scriptDir/fastqi2fastqp.pl $pairedInterleavedTrimmed $outputID$tailID > output_fastqi2fastqp.pl.std 2> output_fastqi2fastqp.pl.err") == 0 or die("error while running $scriptDir/fastqi2fastqp.pl: $!");
-    push @ToBeDeleted, "output_fastqi2fastqp.pl.std";
-    push @ToBeDeleted, "output_fastqi2fastqp.pl.err";
+    fastqi2fastqp($pairedInterleavedTrimmed, "$outputID$tailID");
     
     $read_1 = "$outputID$tailID\_1\.fastq";
     $read_2 = "$outputID$tailID\_2\.fastq";
@@ -351,9 +375,6 @@ sub checkDataConsistency {
             die("file $reads_1 not in correct format: should look like name_1.fastq or name_1.fastq.gz: $!");
         }
     	
-        #print $ID."\n";
-        #print $dirname."\n";
-        #print $suffix."\n";
        
         if($gz == 0) {
 	        $reads_1    = $dirname.$ID."_1.fastq";
@@ -437,8 +458,6 @@ sub countReads {
 sub countReadsSingleFile {
     my ($file) = @_;
     
-    
-    
     if ($file =~ /\.gz$/) {
         open(FASTQ, "gunzip -c $file |") || die "not able to open file $file";
     } else {
@@ -459,28 +478,55 @@ sub countReadsSingleFile {
 }
 
 
+sub fastqi2fastqp {
+    my ($in , $outID) = @_;
+    open(IN, $in);
+    open(OUT1, ">$outID\_1.fastq");
+    open(OUT2, ">$outID\_2.fastq");
+
+    while(my $seq1=<IN>){
+        $seq1 .= <IN>;
+        $seq1 .= <IN>;
+        $seq1 .= <IN>;
+    
+        my $seq2 = <IN>;
+        $seq2 .= <IN>;
+        $seq2 .= <IN>;
+        $seq2 .= <IN>;
+
+        print OUT1 $seq1;
+        print OUT2 $seq2;
+    }
+    close IN;
+    close OUT1;
+    close OUT2;
+}
+
+
+
 __END__
 
-=head1 SciLifeLab-MP-removeDuplicates.pl
+=head1 SciLifeLab-MP-pipeline-application.pl
  
 =head1 SYNOPSIS
  
- SciLifeLab-MP-removeDuplicates.pl --reads fileToProcess_1.fastq [OPTIONS]
- 
+SciLifeLab-MP-pipeline-application.pl --reads mate_pair_1.fastq[.gz]  [--reads mate_pair_1.fastq[.gz]] [--reference PATH_TO_DATA_STRUCTURE] [OPTIONS]
+
  Options (Mandatory)
  
- --reads file containing reads one. Data is assumed to be provided in pairs in two different files with the header but ending _1.fastq and _2.fastq respectively. This option can be repeated sevaral times
+ --reads file containing read one. It is assumed that paired reads are saved in two different files with the same header but ending with _1.fastq[.gz] and _2.fastq[.gz] respectively. This option can be repeated sevaral times
  
   Options (Not mandatory)
---reference
- --hist-width
- --no-adapt-removal
- --no-remove-dupl
+ --aligner   ALIGNER TO BE USED
+ --reference PATH_TO_ALIGNER_DATA_STRUCTURE
+ --hist-width HISTOGRAM WIDTH TO PLOT INSERT SIZE
+ --no-adapt-removal DO NOT REMOVE ADAPTOR
+ --adaptor ADAPTOR_TO_BE_TRIMMED this option can be repeated sever times. It overwrites standard adapter (i.e., Nextera MP)
  --help displays this help message
- --no-clean do not delete intermediate files
 
-
-
+ for more informations please read:
+ http://res.illumina.com/documents/products/technotes/technote_nextera_matepair_data_processing.pdf
+ 
 
 
 
